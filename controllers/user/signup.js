f6715f2ad7a22da356e6660816e882806b76d18a
Tushar Mishra <tusharmishra9695@gmail.com>
2024-02-28 18:03:37 +0530

    #some updates in email and no

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -14,17 +14,24 @@ async function handlePostUser(req, res) {
   try {
     const otpGenerated = generateOTP();
     const expiredOTP = expiresAt;
-    console.log(expiredOTP, expiredOTP, "otp");
     const message = {
       body: `Here is your OTP for signup ${otpGenerated}`,
       from: process.env.FROM_SECRET,
       to: `+91${phoneNumber}`,
     };
-    let findUser = await Signup.findOne({ phoneNumber, email });
+    let findUser = await Signup.findOne({ phoneNumber });
+    let findEmail = await Signup.findOne({ email });
+
     if (findUser && findUser.isVerified) {
       res.status(200).send({
         // 409 previously removed because not working in native
-        message: "Phone no. / Email is already registered.",
+        message: "Phone no. is already registered.",
+        success: false,
+      });
+    } else if (findEmail && findEmail.isVerified) {
+      res.status(200).send({
+        // 409 previously removed because not working in native
+        message: "Email is already registered.",
         success: false,
       });
     } else if (findUser && !findUser.isVerified) {
@@ -34,7 +41,7 @@ async function handlePostUser(req, res) {
       await findUser.save();
       res.send({
         message: `OTP sended to ${phoneNumber} ! Verify First`,
-        success: "true",
+        success: true,
       });
     } else {
       // await client.messages.create(message);  // otp sending
@@ -42,12 +49,13 @@ async function handlePostUser(req, res) {
       result.otp = otpGenerated;
       result.expiresAt = expiredOTP;
       await result.save(); // saving user to db
-      res.status(201).send({
+      res.status(200).send({
         message: `OTP sended to ${phoneNumber}`,
         success: true,
       });
     }
   } catch (e) {
+    console.log(e, "error");
     errorMessage(res, "signup");
   }
 }
